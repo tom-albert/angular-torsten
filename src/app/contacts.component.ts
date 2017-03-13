a1e95d4d94c8909ca7f38aa974adb38ce2fa80d9
Indre <indre.skaburskyte@reservix.de>
2017-03-13 10:54:33 +0100

    ex 2

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Contact } from './models/contact';
 
 @Component({
   selector: 'trm-contacts-app',
-  template: `
-    <md-toolbar color="accent">Contacts</md-toolbar>
-`,
+  templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss']
 })
 export class ContactsAppComponent {
-  title = 'Angular 2 Master Class setup works!';
+    contact: Contact = {
+        id: 6,
+        name: 'Diana Ellis',
+        email: '',
+        phone: '',
+        birthday: '',
+        website: '',
+        image: '/assets/images/6.jpg',
+        address: {
+            street: '6554 park lane',
+            zip: '43378',
+            city: 'Rush',
+            country: 'United States'
+        }
+    }
 }
