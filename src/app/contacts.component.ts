ebd9815ef4d5d597dca55d0bc929889f78eb3092
Indre <indre.skaburskyte@reservix.de>
2017-03-13 13:54:01 +0100

    ex 4 dependency injection

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { Contact } from './models/contact';
 import {ContactsService} from "./contacts.service";
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'trm-contacts-app',
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.scss']
 })
-export class ContactsAppComponent {
+export class ContactsAppComponent implements OnInit {
 
     contacts: Contact[];
 
